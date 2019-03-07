adc17f7e5f448bb2d91ac5c5867153e1b5fa8461
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-03-07 12:46:19 -0800

    Implement part of the color-drawing

--- qtile.cpp
+++ qtile.cpp
@@ -16,8 +16,33 @@ void QTile::draw()
         setStyleSheet("QTile {background- color: yellow, border-width: 10px;}");
     }
     else {
+        switch (tile->get_value()) {
+        case 2:{
+            setStyleSheet("QLabel { background: rgb(237,233,225); color: black; font: bold; border-radius: 10px; font: 40pt; }");
+            break;
+        }
+        case 4:{
+            setStyleSheet("QLabel { background: rgb(237,229,210); color: black; font: bold; border-radius: 10px; font: 40pt; }");
+            break;
+        }
+        case 8:{
+            setStyleSheet("QLabel { background: rgb(239,193,141); color: black; font: bold; border-radius: 10px; font: 60pt; }");
+            break;
+        }
+        case 16:{
+            setStyleSheet("QLabel { background: rgb(239,167,111); color: black; font: bold; border-radius: 10px; font: 60pt; }");
+            break;
+        }
+        case 32:{
+            setStyleSheet("QLabel { background: rgb(237,146,116); color: black; font: bold; border-radius: 10px; font: 60pt; }");
+            break;
+        }
+        case 0: {
+            setStyleSheet("QTile { background: rgb(238,228,218); color: black; font: bold; border-radius: 10px; font: 40pt; }");
+            break;
+        }
+        }
         setText(QString::number(tile->get_value()));
-        this -> setStyleSheet("QLabel { background: rgb(238,228,218); color: rgb(119,110,101); font: bold; border-radius: 10px; font: 60pt; }");
 
     }
 
